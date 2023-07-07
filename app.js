2166fb25379f319ccc91105f4f3ff5d0c201cca6
Amauri22 <mar.hamaekers@gmail.com>
2023-07-07 11:43:02 +0200

    Add pictures

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 "use strict";
-const accountBtn = document.getElementById("account--nav");
-const modal = document.querySelector(".modal");
-const closeBtn = document.querySelector(".close");
-const producten = document.getElementById("producten");
+// const accountBtn = document.getElementById("account--nav");
+// const modal = document.querySelector(".modal");
+// const closeBtn = document.querySelector(".close");
+// const producten = document.getElementById("producten");
+//
+// const featuredProducts = document.getElementById("featured-products");
+//
+// accountBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   console.log("i got clicked");
+//
+//
+//   modal.id = "";
+// });
+//
+// closeBtn.addEventListener("click", function () {
+//   modal.id = "loginModal";
+// });
+//
+// producten.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   featuredProducts.scrollIntoView({behavior:"smooth"})
+// });
 
-const featuredProducts = document.getElementById("featured-products");
-
-accountBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("i got clicked");
-  
-  
-  modal.id = "";
-});
-
-closeBtn.addEventListener("click", function () {
-  modal.id = "loginModal";
-});
-
-producten.addEventListener("click", function (e) {
-  e.preventDefault();
-  featuredProducts.scrollIntoView({behavior:"smooth"})
-});
+console.log("Hello");
+console.log("hey you");
+const test = document.addEventListener(".story__content");
